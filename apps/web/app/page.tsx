22527476fe592323a0f358b2399dd238a2739483
Wataru Sato <awwa@users.noreply.github.com>
2024-04-07 23:53:31 +0900

    first commit

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,7 +1,7 @@
 import { prisma } from "@repo/database";
 
 export default async function IndexPage() {
-  const users = await prisma.user.findMany();
+  const users = await prisma.racer.findMany();
 
   return (
     <div>
---
+++ packages/database/prisma/migrations/20240407144613_v1/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE "Team" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "fullname" TEXT NOT NULL,
+    "shortname" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Racer" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "kana" TEXT NOT NULL,
+    "event" TEXT NOT NULL,
+    "bib" INTEGER NOT NULL,
+    "gender" TEXT NOT NULL,
+    "seed" INTEGER NOT NULL,
+    "teamId" TEXT,
+    "firstParticipate" BOOLEAN NOT NULL,
+    "age" INTEGER,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "Racer_teamId_fkey" FOREIGN KEY ("teamId") REFERENCES "Team" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "RacerResult" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "set" INTEGER NOT NULL,
+    "time" INTEGER,
+    "status" TEXT,
+    "racerId" TEXT NOT NULL,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL,
+    CONSTRAINT "RacerResult_racerId_fkey" FOREIGN KEY ("racerId") REFERENCES "Racer" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Team_fullname_key" ON "Team"("fullname");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Racer_bib_key" ON "Racer"("bib");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "RacerResult_set_racerId_key" ON "RacerResult"("set", "racerId");
